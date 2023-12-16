978d18795d50f2050582d6c75d75df23ea9f3d3b
cesarg <c.egutierrez19@gmail.com>
2023-12-16 17:37:15 -0600

    Added files to main branch

--- pythonProject10/random fact.py
+++ pythonProject10/random fact.py
@@ -1,15 +1,3 @@
-"""
-import randfacts as rf
-
-print('Random facts')
-x = ''
-
-while x == '':
-    print('Press enter for a fact')
-    x = input()
-    fact = rf.get_fact()
-    print(fact)
- """
 
 import randfacts as rf
 
